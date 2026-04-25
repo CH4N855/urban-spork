bfc51d8a7a7f86a2c29e9b38082027b19604e361
longsengly <longsengly320@gmail.com>
2026-04-25 15:54:30 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+
+      <div class="tooltip-wrapper">
+        <button class="hover-me">ចុចលើខ្ងុំមក🎈</button>
+        <div class="tooltip">
+          <span class="star">⭐</span>
+          <span class="star">⭐</span>
+          <span class="star">⭐</span>
+        I love You Ravy😘🩵
+        </div>
+      </div>
+
+  );
+}
+
+export default App;
